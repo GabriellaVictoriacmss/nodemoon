80dc7054dfccbeaccd999dc4fb59853ceaaf76a9
Gabriella Victória <gabriella.victoria@eaportal.org>
2023-11-09 07:57:08 -0300

    nodemon

---
+++ index.js
@@ -0,0 +1,18 @@
+const { application } = require("express")
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const caminhoBase = path.join(__dirname, "templates")
+
+
+
+app.get('/', (requisicao, resposta ) => {
+    resposta.sendFile(`${caminhoBase}/index.html`)
+})
+
+app.listen(3000, () => {
+    console.log("Estou rodando na porta 3000")
+})
+
